966fa3a914c2142649140991cc161be34ebed190
Ayudiahhhhh <diahcrb1@gmail.com>
2024-12-10 09:45:15 +0700

    pw

--- auth/login/index.php
+++ auth/login/index.php
@@ -14,27 +14,28 @@ include('../layouts/link.php');
                         <h3>Sign In</h3>
                         <p>Please sign in to continue to Voler.</p>
                     </div>
+                    
                     <form action="index.html">
-                        <div class="form-group position-relative has-icon-left">
+                    <div class="form-group position-relative has-icon-left">
                             <label for="username">Username</label>
                             <div class="position-relative">
-                                <input type="text" class="form-control" id="username">
+                                <input type="text" class="form-control"name="username" id="username">
                                 <div class="form-control-icon">
-                                    <i data-feather="user"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group position-relative has-icon-left">
+                       <div class="form-group position-relative has-icon-left">
                             <div class="clearfix">
                                 <label for="password">Password</label>
-                                <a href="auth-forgot-password.html" class='float-right'>
+                                <a href="auth-forgot-password.html" class="float-right">
                                     <small>Forgot password?</small>
                                 </a>
                             </div>
                             <div class="position-relative">
-                                <input type="text" class="form-control" id="password">
+                                <input type="password" class="form-control"name="password" id="password">
                                 <div class="form-control-icon">
-                                    <i data-feather="lock"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                                 </div>
                             </div>
                         </div>
