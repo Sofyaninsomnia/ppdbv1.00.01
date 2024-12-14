8530d10dcc1c224d7ed6542519c4a05b6378d689
sofyan-hashh <ngodingsekarepe@gmail.com>
2024-12-14 14:45:05 +0700

    include

--- admin/jurusan/index.php
+++ admin/jurusan/index.php
@@ -3,11 +3,11 @@ include('../../layouts/header.php');
 ?>
 <div id="app">
     <?php
-    include('../../layouts/aside.php');
+    include('../layouts/aside.php');
     ?>
     <div id="main">
         <?php
-        include('../../layouts/nav.php');
+        include('../layouts/nav.php');
         ?>
         <div class="main-content container-fluid">
             <section class="section">
