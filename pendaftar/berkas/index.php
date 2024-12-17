de3bfe539e7440be96634b37809928626a5be0c3
Ayudiahhhhh <diahcrb1@gmail.com>
2024-12-17 10:49:26 +0700

    berkas

--- pendaftar/berkas/index.php
+++ pendaftar/berkas/index.php
@@ -1,5 +1,3 @@
-
-
 <?php
 include('../../layouts/header.php');
 ?>
@@ -12,81 +10,80 @@ include('../layouts/aside.php');
 include('../layouts/nav.php');
 ?>
 <section id="input-file-browser">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">File Input</h4>
-                    </div>
-                    
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-lg-6 col-md-12">
-                                    <p>Default</p>
-                                   <div class="form-file">
-                                        <input type="file" class="form-file-input" id="customFile">
-                                        <label class="form-file-label" for="customFile">
-                                            <span class="form-file-text">Choose file...</span>
-                                            <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 col-md-12">
-                                    <p>Default</p>
-                                   <div class="form-file">
-                                        <input type="file" class="form-file-input" id="customFile">
-                                        <label class="form-file-label" for="customFile">
-                                            <span class="form-file-text">Choose file...</span>
-                                            <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 col-md-12">
-                                    <p>Default</p>
-                                   <div class="form-file">
-                                        <input type="file" class="form-file-input" id="customFile">
-                                        <label class="form-file-label" for="customFile">
-                                            <span class="form-file-text">Choose file...</span>
-                                            <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 col-md-12">
-                                    <p>Default</p>
-                                   <div class="form-file">
-                                        <input type="file" class="form-file-input" id="customFile">
-                                        <label class="form-file-label" for="customFile">
-                                            <span class="form-file-text">Choose file...</span>
-                                            <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 col-md-12">
-                                    <p>Default</p>
-                                   <div class="form-file">
-                                        <input type="file" class="form-file-input" id="customFile">
-                                        <label class="form-file-label" for="customFile">
-                                            <span class="form-file-text">Choose file...</span>
-                                            <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 col-md-12">
-                                    <p>With Icon And Button Color</p>
-                                    <div class="form-file">
-                                        <input type="file" class="form-file-input" id="customFile">
-                                        <label class="form-file-label" for="customFile">
-                                            <span class="form-file-text">Choose file...</span>
-                                            <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
-                                        </label>
-                                    </div>
-                                </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">File Input</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <p>Kartu Keluarga</p>
+                            <div class="form-file">
+                                <input type="file" class="form-file-input" id="customFile">
+                                <label class="form-file-label" for="customFile">
+                                    <span class="form-file-text">upload kartu keluarga...</span>
+                                    <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
+                                </label>
                             </div>
                         </div>
+                        <div class="col-lg-6 col-md-12">
+                            <p>Akta Kelahiran</p>
+                            <div class="form-file">
+                                <input type="file" class="form-file-input" id="customFile">
+                                <label class="form-file-label" for="customFile">
+                                    <span class="form-file-text">upload akta kelahiran...</span>
+                                    <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <p>SKL</p>
+                            <div class="form-file">
+                                <input type="file" class="form-file-input" id="customFile">
+                                <label class="form-file-label" for="customFile">
+                                    <span class="form-file-text">upload surat keterangan lulus...</span>
+                                    <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <p>PasPhoto</p>
+                            <div class="form-file">
+                                <input type="file" class="form-file-input" id="customFile">
+                                <label class="form-file-label" for="customFile">
+                                    <span class="form-file-text">upload foto 3x4...</span>
+                                    <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <p>Ktp Ayah</p>
+                            <div class="form-file">
+                                <input type="file" class="form-file-input" id="customFile">
+                                <label class="form-file-label" for="customFile">
+                                    <span class="form-file-text">upload ktp ayah...</span>
+                                    <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12">
+                            <p>Ktp Ibu</p>
+                            <div class="form-file">
+                                <input type="file" class="form-file-input" id="customFile">
+                                <label class="form-file-label" for="customFile">
+                                    <span class="form-file-text">upload ktp ibu...</span>
+                                    <span class="form-file-button btn-primary "><i data-feather="upload"></i></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 <?php
 include('../../layouts/footer.php');
 ?>
